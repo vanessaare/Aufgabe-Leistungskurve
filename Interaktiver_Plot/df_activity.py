ab35264143f6672087b3fdc627b27c071f2b790f
Vanessa Reich <rv3725@mci4me.at>
2026-05-19 12:04:08 +0200

    verbessert

--- Interaktiver_Plot/df_activity.py
+++ Interaktiver_Plot/df_activity.py
@@ -9,4 +9,7 @@ df_activity = pd.read_csv("activity.csv", skipinitialspace=True)
 #print(df_activity.describe())  # Statistik
 
 Leistung_Mittelwert = df_activity["PowerOriginal"].mean()
-print(Leistung_Mittelwert)
+print("Die mittlere Leistung beträgt: ", Leistung_Mittelwert)
+
+Leistung_Maximalwert = df_activity["PowerOriginal"].max()
+print("Die maximale Leistung beträgt: ", Leistung_Maximalwert)
