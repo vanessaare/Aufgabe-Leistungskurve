e69c6f99a948dab6a5b2fe70d60cd288b947c402
Vanessa Reich <rv3725@mci4me.at>
2026-05-19 13:07:00 +0200

    heartrate zones added

--- Interaktiver_Plot/df_activity.py
+++ Interaktiver_Plot/df_activity.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-df_activity = pd.read_csv("activity.csv", skipinitialspace=True)
+df_activity = pd.read_csv("data/activity.csv", skipinitialspace=True)
+df_activity["Time"] = df_activity.index
 
-#print(df_activity.tail())
-#print(df_activity.info())      # Struktur
-#print(df_activity.describe())  # Statistik
 
 Leistung_Mittelwert = df_activity["PowerOriginal"].mean()
 print("Die mittlere Leistung beträgt: ", Leistung_Mittelwert)
@@ -17,15 +15,56 @@ Leistung_Maximalwert = df_activity["PowerOriginal"].max()
 print("Die maximale Leistung beträgt: ", Leistung_Maximalwert)
 
 
+df_activity["PowerOriginal"] = df_activity["PowerOriginal"].fillna(0)
 df_activity = df_activity.dropna(subset=["HeartRate"])
-df_activity = df_activity.dropna(subset=["PowerOriginal"])
 
-# Zeitachse bauen (jede Zeile = 1 Sekunde)
-df_activity["Time"] = df_activity.index
+
+# Herzfrequenz-Zonen definieren
 
 fig = go.Figure()
 
-# Herzfrequenz
+max_hr = 450
+
+zones = [
+    0,
+    0.5 * max_hr,
+    0.6 * max_hr,
+    0.7 * max_hr,
+    0.8 * max_hr,
+    1.0 * max_hr
+]
+
+colors = [
+    "rgba(0,255,0,0.05)",
+    "rgba(173,255,47,0.05)",
+    "rgba(255,255,0,0.05)",
+    "rgba(255,165,0,0.05)",
+    "rgba(255,0,0,0.05)"
+]
+
+for i in range(len(zones)-1):
+    fig.add_shape(
+        type="rect",
+        x0=0,
+        x1=df_activity["Time"].max(),
+        y0=zones[i],
+        y1=zones[i+1],
+        fillcolor=colors[i],
+        opacity=0.7,
+        layer="below",
+        line_width=0,
+    )
+
+labels = ["Z1", "Z2", "Z3", "Z4", "Z5"]
+
+df_activity["Zone"] = pd.cut(df_activity["HeartRate"], bins=zones, labels=labels)
+
+time_in_zone = df_activity["Zone"].value_counts().sort_index()
+print("Zeit in Zonen (Sekunden):")
+print(time_in_zone)
+
+#fig = go.Figure()
+
 fig.add_trace(go.Scatter(
     x=df_activity["Time"],
     y=df_activity["HeartRate"],
@@ -33,7 +72,6 @@ fig.add_trace(go.Scatter(
     name="Heart Rate"
 ))
 
-# Leistung (Power)
 fig.add_trace(go.Scatter(
     x=df_activity["Time"],
     y=df_activity["PowerOriginal"],
@@ -47,6 +85,12 @@ fig.update_layout(
     yaxis_title="Wert"
 )
 
-fig.show()
+fig.update_layout(
+    title="Herzfrequenz & Leistung über Zeit",
+    xaxis_title="Zeit (s)",
+    yaxis_title="Wert",
+    plot_bgcolor="white"
+)
+fig.update_yaxes(range=[0, df_activity["HeartRate"].max() * 1.05])
 
-df_activity
+fig.show()
