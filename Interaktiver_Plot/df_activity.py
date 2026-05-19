c1d6f298f411ed1948d2b1aa138a6d44b6b175dd
Vanessa Reich <rv3725@mci4me.at>
2026-05-19 13:09:22 +0200

    heartrate zones added

--- Interaktiver_Plot/df_activity.py
+++ Interaktiver_Plot/df_activity.py
@@ -4,11 +4,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-df_activity = pd.read_csv("activity.csv", skipinitialspace=True)
+df_activity = pd.read_csv("data/activity.csv", skipinitialspace=True)
+df_activity["Time"] = df_activity.index
 
-#print(df_activity.tail())
-#print(df_activity.info())      # Struktur
-#print(df_activity.describe())  # Statistik
 
 Leistung_Mittelwert = df_activity["PowerOriginal"].mean()
 print("Die mittlere Leistung beträgt: ", Leistung_Mittelwert)
@@ -17,11 +15,11 @@ Leistung_Maximalwert = df_activity["PowerOriginal"].max()
 print("Die maximale Leistung beträgt: ", Leistung_Maximalwert)
 
 
+df_activity["PowerOriginal"] = df_activity["PowerOriginal"].fillna(0)
 df_activity = df_activity.dropna(subset=["HeartRate"])
-df_activity = df_activity.dropna(subset=["PowerOriginal"])
 
-# Zeitachse bauen (jede Zeile = 1 Sekunde)
-df_activity["Time"] = df_activity.index
+
+# Herzfrequenz-Zonen definieren
 
 fig = go.Figure()
 
@@ -68,14 +66,14 @@ print(time_in_zone)
 #fig = go.Figure()
 
 fig.add_trace(go.Scatter(
-    x=df_activity["Duration"],
+    x=df_activity["Time"],
     y=df_activity["HeartRate"],
     mode="lines",
     name="Heart Rate"
 ))
 
 fig.add_trace(go.Scatter(
-    x=df_activity["Duration"],
+    x=df_activity["Time"],
     y=df_activity["PowerOriginal"],
     mode="lines",
     name="Power"
@@ -83,8 +81,16 @@ fig.add_trace(go.Scatter(
 
 fig.update_layout(
     title="Herzfrequenz & Leistung über Zeit",
-    xaxis_title="Zeit in ms",
-    yaxis_title="Messwerte in mV"
+    xaxis_title="Zeit (s)",
+    yaxis_title="Wert"
 )
+
+fig.update_layout(
+    title="Herzfrequenz & Leistung über Zeit",
+    xaxis_title="Zeit (s)",
+    yaxis_title="Wert",
+    plot_bgcolor="white"
+)
+fig.update_yaxes(range=[0, df_activity["HeartRate"].max() * 1.05])
 
 fig.show()
