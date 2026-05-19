384051cf8113b9cca174f0bc3a5dd3137732890b
lraffler09 <rl9100@mci4me.at>
2026-05-19 12:44:50 +0200

    added skipintialspace

--- Interaktiver_Plot/df_activity.py
+++ Interaktiver_Plot/df_activity.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-df_activity = pd.read_csv("activity.csv", skipinitialspace=True)
-
+df_activity = pd.read_csv("activity.csv", skipinitialspace=True, usecols=["Duration", "HeartRate", "PowerOriginal"])
+df_activity = df_activity.dropna(subset=["HeartRate", "PowerOriginal"])  # Entfernen von Zeilen mit fehlenden Werten
 #print(df_activity.tail())
 #print(df_activity.info())      # Struktur
 #print(df_activity.describe())  # Statistik
@@ -17,17 +17,15 @@ Leistung_Maximalwert = df_activity["PowerOriginal"].max()
 print("Die maximale Leistung beträgt: ", Leistung_Maximalwert)
 
 
-df_activity = df_activity.dropna(subset=["HeartRate"])
-df_activity = df_activity.dropna(subset=["PowerOriginal"])
 
 # Zeitachse bauen (jede Zeile = 1 Sekunde)
-df_activity["Time"] = df_activity.index
+df_activity["Duration"] = df_activity.index
 
 fig = go.Figure()
 
 # Herzfrequenz
 fig.add_trace(go.Scatter(
-    x=df_activity["Time"],
+    x=df_activity["Duration"],
     y=df_activity["HeartRate"],
     mode="lines",
     name="Heart Rate"
@@ -35,7 +33,7 @@ fig.add_trace(go.Scatter(
 
 # Leistung (Power)
 fig.add_trace(go.Scatter(
-    x=df_activity["Time"],
+    x=df_activity["Duration"],
     y=df_activity["PowerOriginal"],
     mode="lines",
     name="Power"
@@ -43,9 +41,10 @@ fig.add_trace(go.Scatter(
 
 fig.update_layout(
     title="Herzfrequenz & Leistung über Zeit",
-    xaxis_title="Zeit (s)",
-    yaxis_title="Wert"
+    xaxis_title="Zeit in ms",
+    yaxis_title="Messwerte in mV"
 )
+
 
 fig.show()
 
